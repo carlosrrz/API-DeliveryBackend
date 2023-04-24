e1333f357437ae600455b83d1146770e586b209d
Carlos Ruiz <carlossruizr@gmail.com>
2023-04-23 20:00:16 -0500

    first commit

---
+++ src/main/java/pe/edu/upao/apideliverybackend/ApiDeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.apideliverybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeliveryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDeliveryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upao/apideliverybackend/ApiDeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.apideliverybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeliveryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
